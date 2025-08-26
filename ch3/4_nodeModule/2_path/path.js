73f640566bd892bc6a81e4b842396894ed43f556
dhtlsxks69 <dhtlsxks69@naver.com>
2025-08-26 17:24:30 +0900

    코드 폴더명 변경

--- ch3/4_nodeModule/2_path/path.js
+++ ch3/4_nodeModule/2_path/path.js
@@ -12,15 +12,15 @@ console.log('path.basename - extname:', path.basename(string, path.extname(strin
 console.log('------------------------------');
 console.log('path.parse()', path.parse(string)); // 파일 경로를 root, dir, base, ext, name으로 분리
 console.log('path.format():', path.format({ // path.parse()한 객체를 파일 경로로 합침
-    dir: 'C:\\Nodejs\\ch3-1\\4_nodeModule\\2_path',
+    dir: 'C:\\Nodejs\\ch3\\4_nodeModule\\2_path',
     name: 'path',
     ext: '.js',
 }));
-console.log('path.normalize():', path.normalize('C://Nodejs\\\\ch3-1\\\\4_nodeModule\\\\2_path\\\\path.js')); // /나 \를 실수로 여러 번 사용했거나 혼용했을 때 정상적인 경로로 변환
+console.log('path.normalize():', path.normalize('C://Nodejs\\\\ch3\\\\4_nodeModule\\\\2_path\\\\path.js')); // /나 \를 실수로 여러 번 사용했거나 혼용했을 때 정상적인 경로로 변환
 console.log('------------------------------');
 console.log('path.isAbsolute(C:\\):', path.isAbsolute('C:\\')); // 파일의 경로가 절대경로인지 상대경로인지를 true, false로 구분
 console.log('path.isAbsolute(./home):',path.isAbsolute('./home'));
 console.log('------------------------------');
-console.log('path.relative():', path.relative('C:\\Nodejs\\ch3-1\\4_nodeModule\\2_path\\path.js', 'C:\\')); // 경로를 두 개 넣으면 첫 번째 경로에서 두 번째 경로로 가는 방법을 알려줌
+console.log('path.relative():', path.relative('C:\\Nodejs\\ch3\\4_nodeModule\\2_path\\path.js', 'C:\\')); // 경로를 두 개 넣으면 첫 번째 경로에서 두 번째 경로로 가는 방법을 알려줌
 console.log('path.join():', path.join(__dirname, '..', '..', '/4_nodeModule', '.', '/2_path')); // 여러 인수를 넣으면 하나의 경로로 합침, 상대경로인..(부모디렉터리)과 .(현 위치)도 알아서 처리
 console.log('path.resolve():', path.resolve(__dirname, '..', '4_nodeModule', '.', '/2_path')); // path.join()과 비슷하지만 차이가 있다.
